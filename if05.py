8d309e5b9902ea9ad92b3a279117c1a7197322c3
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-10-20 19:23:05 +0500

    5 misol

--- if05.py
+++ if05.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n%10 
+    b=(n%100)//10
+    c=(n%1000)//100
+    d=(n%10000)//1000
+    f=(n//100000)
+    m=a
+    if m<b:
+        m=b
+    if m<c:
+        m=c
+    if m<d:
+        m=d
+    if m<f:
+        m=f
+    return int(m)
+print(main(23456))
